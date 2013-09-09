780a5186bdbce9ee5b1276517dd7d9d5fba7b15a
andy c <t2ahc@sad-panda.local>
2013-09-09 19:18:23 -0400

    rename internal variables for readability

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,17 +1,17 @@
 (ns game-of-life.core)
 
-(defn conways-rule [a n]
-  (or (and a (or (= n 2) (= n 3)))
-      (and (not a) (= n 3))))
+(defn conways-rule [alive number-of-neighbors]
+  (or (and alive (or (= number-of-neighbors 2) (= number-of-neighbors 3)))
+      (and (not alive) (= number-of-neighbors 3))))
 
 (defn generate-neighbors [[x y]]
   (for [dx [-1 0 1] dy [-1 0 1] :when (not= [dx dy] [0 0])]
     [(+ x dx) (+ y dy)]))
 
 (defn count-adjacent-cells [xys]
-  (let [neighbors (mapcat generate-neighbors xys)
-        adder (fn [m k] (assoc m k (inc (get m k 0))))]
-    (reduce adder {} neighbors)))
+  (let [all-neighbors (mapcat generate-neighbors xys)
+        increment-map-at-key (fn [m k] (assoc m k (inc (get m k 0))))]
+    (reduce increment-map-at-key {} all-neighbors)))
 
 (defn update-cells [f m s]
   (apply hash-set (for [[k n] m :let [a (contains? s k)] :when (f a n)] k)))
