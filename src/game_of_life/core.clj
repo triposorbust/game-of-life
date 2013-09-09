0ecac5c3493781934e0ab28cac13de4fab80e1c6
andy c <t2ahc@sad-panda.local>
2013-09-09 13:15:34 -0400

    build propagation rules!

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,6 +1,18 @@
 (ns game-of-life.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; render :: set of live cells -> void
+;;
+;; Renders the live cells.
+;; 
+
+(defn next-lives [m s]
+  (let [adjacent-cells  (fn [k] (get m k 0))
+        alive?          (fn [k] (contains? s k))
+        survives?       (fn [k] (let [n (adjacent-cells k)]
+                                  (and (alive? k) (or (= n 2) (= n 3)))))
+        newly-born?     (fn [k] (let [n (adjacent-cells k)]
+                                  (and (not (alive? k)) (= n 3))))]
+    (apply hash-set (for [[k _] m :when (or (survives? k) (newly-born? k))] k))))
+
+(defn -main [& args]
+  nil)
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-next-lives
+  (testing "Basic rules for cell initiation / propagation / termination."
+
+    ;; Termination (as if by underpopulation).
+    (is (empty? (next-lives {:cell 0} #{:cell})))
+    (is (empty? (next-lives {:cell 1} #{:cell})))
+
+    ;; Propagation of living cells.
+    (is (contains? (next-lives {:cell 2} #{:cell}) :cell))
+    (is (contains? (next-lives {:cell 3} #{:cell}) :cell))
+
+    ;; Termination (as if by overpopulation).
+    (is (empty? (next-lives {:cell 4} #{:cell})))
+
+    ;; Initiation of new cells.
+    (is (contains?  (next-lives {:cell 3} #{}) :cell))))
